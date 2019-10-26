a3f7e3b72fa739575ae12e6330c964ce52d2e63a
EdemAMEGBETO <38034025+EdemAMEGBETO@users.noreply.github.com>
2019-10-26 12:28:39 +0200

    ajout des repositories

---
+++ src/main/java/com/urbanproduction/contact/Contact/ContactRepository.java
@@ -0,0 +1,6 @@
+package com.urbanproduction.contact.Contact;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+}
---
+++ src/main/java/com/urbanproduction/contact/User/UserRepository.java
@@ -0,0 +1,6 @@
+package com.urbanproduction.contact.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
